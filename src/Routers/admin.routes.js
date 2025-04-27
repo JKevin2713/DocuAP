1e671f1fd6d1ba7ec99bc7eb61c33ffb61d47d9a
JKevin2713 <101783066+JKevin2713@users.noreply.github.com>
2025-04-26 19:59:55 -0600

    comment routes

--- src/Routers/admin.routes.js
+++ src/Routers/admin.routes.js
@@ -1,3 +1,6 @@
+/** 
+ * se importan desde el controlador las funciones necesarias para el funcionamiento del sistema
+*/
 import {Router} from "express"
 import { obtenerUsuarios,
         actualizarRol,
@@ -11,21 +14,34 @@ import { obtenerUsuarios,
         monitoreoAsistencia
         } from "../Controllers/admin.Controller.js"
 
+// se define el router para las rutas de administrador
+//---------------------------------------------------------------------------------------------------------------
 const router = Router()
 
+//---------------------------------------------------------------------------------------------------------------
+// Rutas para la gestión de usuarios y ofertas
+//---------------------------------------------------------------------------------------------------------------
+// Obtener datos de usuarios
 router.get("/admin/obtenerDatosUsuarios", obtenerUsuarios);
+// Actualizar rol de usuario
 router.put("/admin/ActualizarRol", actualizarRol)
+// Actualizar datos de usuario
 router.put("/admin/ActualizarUsuario", actualizarUsuario)
+// Eliminar usuario
 router.delete("/admin/EliminarUsuario", eliminarUsuario);
+// Obtener lista de carreras
 router.get('/admin/carreras', obtenerCarreras);
-
+// Obtener lista de ofertas
 router.get('/admin/Ofertas', obtenerOfertas); 
+// Aceptar oferta
 router.put('/admin/aceptarOferta', aceptarOferta); 
+// Eliminar oferta
 router.delete('/admin/eliminarOferta', eliminarOferta); 
+// Actualizar oferta
 router.put('/admin/actualizarOferta', actualizarOferta);
-
+// Monitoreo de asistencia
 router.get('/admin/monitoreoAsistencia', monitoreoAsistencia);
-
-
+//---------------------------------------------------------------------------------------------------------------
+// Exportación del router para ser utilizado en el servidor principal
 
 export default router;
--- src/Routers/profesores.routes.js
+++ src/Routers/profesores.routes.js
@@ -1,3 +1,4 @@
+// Se importan las funciones del controlador de profesores
 import { Router } from "express";
 import {
     getInfoProfesores, 
@@ -22,27 +23,53 @@ import {
     } 
 from "../Controllers/profesores.Controller.js";
 
+// Se inicializa el router para definir las rutas
+//---------------------------------------------------------------------------------------------------------------
 const router = Router()
 
+//---------------------------------------------------------------------------------------------------------------
+// Rutas para la gestión de información de profesores y ofertas
+// ---------------------------------------------------------------------------------------------------------------
+// Obtener información de profesores
 router.get('/infoProfesores/:id', getInfoProfesores); 
+// Actualizar información de profesores
 router.patch('/updateInfoProfesores/:id', updateInfoProfesores);
+// Obtener cursos relacionados con el profesor
 router.get('/getCursos/:id', getAllCourses);
+// Obtener historial de asistencias del profesor
 router.get('/getHistorial/:id', getAllHistorial);
+// Obtener información de usuario relacionada con asistencias
 router.get('/getUserInfoByAsistencias/:id', getUserInfoByAsistencias);
+// Insertar nueva oferta
 router.post('/insertNewOferta/:id', insertNewOferta);
+// Obtener asistencias relacionadas con el profesor
 router.get('/getAsistenciasByProfesor/:id', getAsistenciasByProfesor);
+// Obtener solicitudes relacionadas con asistencias
 router.get('/getSolicitudesRelacionadasConAsistencias/:id', getSolicitudesRelacionadasConAsistencias);
+// Actualizar oferta
 router.patch('/updateOferta/:id', updateOferta);
+// Eliminar oferta
 router.delete('/deleteOferta/:id', deleteOferta);
+// Cerrar oferta
 router.patch('/closeOferta/:id', closeOferta); 
+// Agregar desempeño a la oferta
 router.patch('/addDesempeno/:id', addDesempeno);
+// Buscar carrera por ID de usuario
 router.get('/searchCarreraByuserId/:id', searchCarreraByuserId);
+// Actualizar acciones de postulación
 router.patch('/updatePostulacionAcciones/:userId/', updatePostulacionAcciones);
+// Actualizar feedback de asistencia
 router.patch('/updateAsistenciaFeedback/:type/:id', updateAsistenciaFeedback);
+// Actualizar seguimiento de asistencias
 router.patch('/assignAndRemoveSolicitud', assignAndRemoveSolicitud);
+// Actualizar solicitud de reunión
 router.patch('/setSolicitudReunion/:id', setSolicitudReunion);
+// Rechazar postulación
 router.patch('/rechazarPostulacion/:id/', rechazarPostulacion);
+// Actualizar seguimiento de solicitudes
 router.patch('/updateSeguimiento/:id', updateSeguimiento);
 
-
+//---------------------------------------------------------------------------------------------------------------
+// Exportación del router para ser utilizado en el servidor principal
+//---------------------------------------------------------------------------------------------------------------
 export default router;
